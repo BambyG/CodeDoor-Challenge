a2eb96425b17c9551e504b739562559621978bfe
Bamby <bamby.gassama@gmail.com>
2019-01-05 09:33:59 +0100

    Applying Rashfael changes request

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function dropdownMenu() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.closest('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
